a95cb8d4bb4b080b4c2e293967b75e5e7f5445b5
theDanielTo <dto1989@gmail.com>
2021-06-09 19:18:00 -0700

    Declared favoriteDrinks array | set favoriteDrinks to save to localStorage | change all DOM element variable names to start with $. #6.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,10 @@
 /* exported data */
+var favoriteDrinks = [];
+
+const previousDrinksJSON = window.localStorage.getItem('drinks-local-storage');
+if (previousDrinksJSON !== null) favoriteDrinks = JSON.parse(previousDrinksJSON);
+
+window.addEventListener('beforeunload', function () {
+  const todosJSON = JSON.stringify(favoriteDrinks);
+  window.localStorage.setItem('drinks-local-storage', todosJSON);
+});
